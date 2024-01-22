09266bd40c7b44c3802e8136495130bbfffbb51c
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-01-22 13:08:01 -0500

    Only log violations for ordered groups with a path

--- Sources/XCLinting/Rules/GroupsAreSortedRule.swift
+++ Sources/XCLinting/Rules/GroupsAreSortedRule.swift
@@ -2,33 +2,37 @@ import Foundation
 import XcodeProj
 
 func groupsAreSortedRule(_ environment: XCLinter.Environment) -> [Violation] {
-    var violations = [Violation]()
-    for group in environment.project.pbxproj.groups {
-        violations.append(contentsOf: validateGroupIsSorted(group))
-    }
-    return violations
+	var violations = [Violation]()
+	for group in environment.project.pbxproj.groups {
+		violations.append(contentsOf: validateGroupIsSorted(group))
+	}
+	return violations
 }
 
 private func validateGroupIsSorted(_ group: PBXGroup) -> [Violation] {
-    var violations = [Violation]()
-    let children = group.children.compactMap(\.path)
-    let sortedChildren = children.sorted { lhs, rhs in
-        lhs.compare(
-            rhs,
-            options: [
-                .numeric,
-                .caseInsensitive,
-                .widthInsensitive,
-                .forcedOrdering
-            ],
-            locale: .current
-        ) == .orderedAscending
-    }
-    if children != sortedChildren {
-        violations.append(.init("Group \"\(group.path ?? "???")\" contains unsorted children"))
-    }
-    for childGroup in group.children.compactMap({ $0 as? PBXGroup }) {
-        violations.append(contentsOf: validateGroupIsSorted(childGroup))
-    }
-    return violations
+	var violations = [Violation]()
+	let children = group.children.compactMap(\.path)
+	let sortedChildren = children.sorted { lhs, rhs in
+		lhs.compare(
+			rhs,
+			options: [
+				.numeric,
+				.caseInsensitive,
+				.widthInsensitive,
+				.forcedOrdering
+			],
+			locale: .current
+		) == .orderedAscending
+	}
+	
+	// some groups have no path, like the auto-generated "Products". Let's skip those, as they appear to not even always show up in the UI.
+	if children != sortedChildren, let path = group.path {
+		violations.append(.init("Group \"\(path)\" contains unsorted children"))
+	}
+
+	for childGroup in group.children.compactMap({ $0 as? PBXGroup }) {
+		violations.append(contentsOf: validateGroupIsSorted(childGroup))
+	}
+
+	return violations
 }
